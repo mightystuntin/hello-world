2f5cd0dc89bc576769a7088e2d023d8c9a0539cb
ebenezerhailu <89481847+ebenezerhailu@users.noreply.github.com>
2021-08-24 15:51:54 -0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,19 @@
+/*******************************
+* Hello.java
+*******************************
+* Ebenezer Hailu
+* Aug 24, 2021
+* CMSC 255 004
+*******************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Ebenezer Hailu");
+		System.out.println("Hello.java");
+		System.out.println("This is my first script");
+		System.out.println("Aug. 24, 2021");
+		System.out.println("CMSC 255 004");
+		System.out.println("");
+	}
+}
